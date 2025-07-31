2c0ae2a52f75eb8f8d1df408ae3a53c1d128af79
lonely <3060900582@qq.com>
2025-07-31 19:57:50 +0800

    feat: setup antd and icon

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,13 @@
+import { StepForwardOutlined } from '@ant-design/icons'
+import { Button } from 'antd'
+
 function App(): React.JSX.Element {
-  return <div data-testid="app-wrapper">hello world</div>
+  return (
+    <div data-testid="app-wrapper">
+      <Button type="primary">Primary Button</Button>
+      <StepForwardOutlined />
+    </div>
+  )
 }
 
 export default App
